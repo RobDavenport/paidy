18a31831747d55173e6b5b07122bd782a9530f14
RobDavenport <zerve0@hotmail.com>
2024-11-12 18:45:18 +0900

    add notes, begin shared lib types

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,3 +7,19 @@ async fn main() {
 
     warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
+
+// TODO:
+// Client: add one or more items with a table number,
+// The application MUST, upon creation request, store the item, the table number, and how long the item will take to cook.
+
+// TODO:
+// Client: remove an item for a table,
+// The application MUST, upon deletion request, remove a specified item for a specified table number.
+
+// TODO:
+// Client: query the items still remaining for a table.
+// The application MUST, upon query request, show all items for a specified table number.
+
+// TODO:
+// Client: query a specific item remaining for a table
+// The application MUST, upon query request, show a specified item for a specified table number.
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,1 +1,15 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Menu {
+    items: Vec<MenuItem>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MenuItem {
+    id: u64,
+    name: String,
+    price: f32,
+    prep_min_secs: f32,
+    prep_max_secs: f32,
+}
